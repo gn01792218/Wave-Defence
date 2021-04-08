86a4f3c4e5fc96320bf7f0b781bb515c83e97978
DESKTOP-AS0I9UU\ANDY <xup6910615@gmail.com>
2021-04-08 17:56:44 +0800

    註解getTarget

--- src/gameobj/Tank1.java
+++ src/gameobj/Tank1.java
@@ -14,21 +14,21 @@ public class Tank1 extends Actor{
         this.dir=1;
     }
 
-    public GameObject getTarget(ArrayList<GameObject> enemys){
-        GameObject target = null;
-        int min = 999999999;
-        for(int i=0;i<enemys.size();i++){
-            int xRange = enemys.get(i).collider().centerX() - this.collider().centerX();
-            int yRange = enemys.get(i).collider().centerY() - this.collider().centerY();
-            int c = xRange*xRange+yRange*yRange;
-            //如果C邊
-            if(c<min){
-                target = enemys.get(i);
-                min = c;
-            }
-        }
-        return target;
-    }
+//    public GameObject getTarget(ArrayList<GameObject> enemys){
+//        GameObject target = null;
+//        int min = 999999999;
+//        for(int i=0;i<enemys.size();i++){
+//            int xRange = enemys.get(i).collider().centerX() - this.collider().centerX();
+//            int yRange = enemys.get(i).collider().centerY() - this.collider().centerY();
+//            int c = xRange*xRange+yRange*yRange;
+//            //如果C邊
+//            if(c<min){
+//                target = enemys.get(i);
+//                min = c;
+//            }
+//        }
+//        return target;
+//    }
 
     @Override
     public void paint(Graphics g) {

cd85b16f197e9dfc7baa7364bd2458c2cc4f464b
MSI\gn01792218 <gn01792218@hotmail.com>
2021-04-12 16:08:40 +0800

    印出倒數10秒的數字，並修正敵軍的出現時間

--- src/scene/GameScene.java
+++ src/scene/GameScene.java
@@ -29,14 +29,20 @@ public class GameScene extends Scene {
     private ArrayList<Actor> enemys; //敵軍
     private static Flag flag; //指揮旗
     private Delay delayEnemyBron; //目前用來控制敵人重新生成的間隔時間
-    private Delay delayRound;//回合前30秒的delay
+    private Delay delayRound;//回合前X秒的delay
+    private Delay delayCount;//倒數10秒的週期播放
+    private int countNum; //倒數的播放號碼
+    private boolean enemysMove; //敵軍是否可以移動
 
     @Override
     public void sceneBegin() {
-        image = ImageController.getInstance().tryGet("/m2.png");
+        image = ImageController.getInstance().tryGet("/m2.png"); //場景圖
+        image3 = ImageController.getInstance().tryGet("/count.png"); //倒數的圖片
         delayEnemyBron = new Delay(240);//目前用來控制敵人重新生成的時間
         delayRound = new Delay((600)); //開場前delay前10秒
         delayRound.play();//開場就倒數
+        delayCount = new Delay(60);
+        delayCount.loop();//倒數計時每1秒觸發一次換圖片
         alliance = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             if (i < 5) {
@@ -46,11 +52,17 @@ public class GameScene extends Scene {
             }
         }
         enemys = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {  //第一波敵人
+            enemys.add(new Enemy1(Global.random(400, 1000), Global.random(200, 350), true));
+        }
+        enemysMove=false; //剛開始敵軍不能移動
         flag = new Flag(1, 1, 50, 50, 1, 1, 50, 50);
     }
+
     @Override
     public void sceneEnd() {
     }
+
     @Override
     public CommandSolver.KeyListener keyListener() {
         return null;
@@ -80,20 +92,27 @@ public class GameScene extends Scene {
             }
         };
     }
-
     @Override
     public void paint(Graphics g) {
         g.drawImage(image, 0, -150, null);
+        if (delayCount.count()) {  //每1秒播放圖片
+            countNum++;
+        }
+        int tx =this.countNum*74;  //0-74 1-74*2 2-74*3
+        g.drawImage(image3, 750, 100, 750 + 74, 100 + 90,
+                tx,0,tx+74,90, null); //倒數的圖片
         for (int i = 0; i < alliance.size(); i++) {
             alliance.get(i).paint(g);
         }
         for (int i = 0; i < enemys.size(); i++) {
-            enemys.get(i).paint(g);
+            if(enemysMove) { //敵軍可以移動時才畫
+                enemys.get(i).paint(g);
+            }
         }
         if (flag.isFlagUsable()) {
             flag.paint(g); //旗子可以使用的時候才畫出來
         }
-        if (alliance.size() <= 0) {
+        if (alliance.size() <= 0) { //死光時畫失敗畫面
             image2 = ImageController.getInstance().tryGet("/fail2.png");
             g.drawImage(image2, 350, 250, null);
         }
@@ -115,26 +134,29 @@ public class GameScene extends Scene {
                 break;
             }
         }
-        if (delayRound.count()) { //開場20秒後
+        if (delayRound.count()) { //開場10秒後
             flag.setFlagUsable(false); //旗子不能用
+            enemysMove=true; //10秒後敵軍可以移動
         }
-        //測試用:假如敵軍全消滅，再生成敵軍出來
-        if (enemys.size() == 0) {
-            delayEnemyBron.play();
-            if (delayEnemyBron.count()) {
-                for (int i = 0; i < 10; i++) {
-                    enemys.add(new Enemy1(Global.random(400, 1000), Global.random(200, 350), true));
+        if(enemysMove) { //敵軍可以移動時
+            //敵軍update
+            for (int i = 0; i < enemys.size(); i++) {
+                enemys.get(i).autoAttack(alliance);
+                enemys.get(i).update();
+                if (!enemys.get(i).isAlive()) {
+                    enemys.remove(i);
+                    i--;
+                    break;
                 }
             }
-        }
-        //敵軍update
-        for (int i = 0; i < enemys.size(); i++) {
-            enemys.get(i).autoAttack(alliance);
-            enemys.get(i).update();
-            if (!enemys.get(i).isAlive()) {
-                enemys.remove(i);
-                i--;
-                break;
+            //測試用:假如敵軍全消滅，再生成敵軍出來
+            if (enemys.size() == 0) {
+                delayEnemyBron.play();
+                if (delayEnemyBron.count()) {
+                    for (int i = 0; i < 10; i++) {
+                        enemys.add(new Enemy1(Global.random(400, 1000), Global.random(200, 350), true));
+                    }
+                }
             }
         }
 

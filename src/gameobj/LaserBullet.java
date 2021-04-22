bf6234d8c57b524e48281afa567820afc13b39ae
MSI\gn01792218 <gn01792218@hotmail.com>
2021-04-22 10:44:36 +0800

    //更新雷射車的子彈

--- src/gameobj/LaserBullet.java
+++ src/gameobj/LaserBullet.java
@@ -7,5 +7,6 @@ public class LaserBullet extends Bullet{
     public LaserBullet(float x, float y, float targetX, float targetY) {
         super(x,y,1,1,targetX,targetY);
         this.image1 = ImageOperate.rotateImage(ImageController.getInstance().tryGet("/LaserBullet.png"),trigonometric.getDegree());
+        this.image2=ImageController.getInstance().tryGet("/laserBoom.png");
     }
 }
